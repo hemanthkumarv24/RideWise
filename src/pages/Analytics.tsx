d6ec1f942baaa7015c5c4eb05621bfe0ea6ae60b
Hemanth kumar V <hemanthk_student@ksit.edu.in>
2024-07-25 13:20:20 +0530

    changed width to mobile width

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,4 +1,4 @@
-import  { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import { Card, Col, Row } from "antd";
 import Navbar from "../components/Navbar/Navbar";
@@ -11,13 +11,13 @@ import AverageRatings from "../components/Analytics/AverageRatings";
 const Analytics = () => {
   const [data, setData] = useState({
     total_trips_by_service: [],
-    average_ratings:[],
+    average_ratings: [],
     average_price_by_service: [],
     average_rating_by_service: [],
     peak_usage_times: [],
     peak_usage_times_per_service: [],
     popular_routes: [],
-    service_usage_by_vehicle:[],
+    service_usage_by_vehicle: [],
     service_comparison: { trip_data: [], rating_data: [] },
   });
 
@@ -39,27 +39,29 @@ const Analytics = () => {
 
   // Extracting data for DonutChart components
   const avg = data.service_comparison.trip_data;
-  const serviceNames = avg.map((trip) => trip['service_name']);
-  const distanceData = avg.map((trip) => trip['avg_distance']);
-  const durationData = avg.map((trip) => trip['avg_duration']);
-  const priceData = avg.map((trip) => trip['avg_price']);
+  const serviceNames = avg.map((trip) => trip["service_name"]);
+  const distanceData = avg.map((trip) => trip["avg_distance"]);
+  const durationData = avg.map((trip) => trip["avg_duration"]);
+  const priceData = avg.map((trip) => trip["avg_price"]);
+
+  const responsiveStyle = {
+    display: "flex",
+    justifyContent: "center",
+    width: "90%",
+    '@media (max-width: 768px)': {
+      width: "100%",
+    },
+  };
 
   return (
     <>
       <Navbar />
 
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          // minHeight: "100vh",
-          marginTop: "20px",
-        }}
-      >
-        <Row gutter={10} style={{ width: "80%" }}>
+      <div style={{ display: "flex", justifyContent: "center"}}>
+        <Row gutter={10} style={responsiveStyle}>
           {data.total_trips_by_service.map((trip, index) => (
             <Col key={index} xs={24} md={12} lg={8} xl={8}>
-              <Card style={{marginTop:'10px'}}>
+              <Card style={{ marginTop: "10px" }}>
                 <h2>{trip["service_name"]}</h2>
                 <h3>Total Trips: {trip["count"]}</h3>
               </Card>
@@ -67,90 +69,58 @@ const Analytics = () => {
           ))}
         </Row>
       </div>
-      
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          // marginTop: "20px",
-        }}
-      >
-        <Row gutter={10} style={{ width: "80%" }}>
+
+      <div style={{ display: "flex", justifyContent: "center",  }}>
+        <Row gutter={10} style={responsiveStyle}>
           <Col xs={24} md={8} lg={8} xl={8}>
-            <Card title="Average Distance" style={{marginTop:'10px'}}>
+            <Card title="Average Distance" style={{ marginTop: "10px" }}>
               <h2></h2>
               <DonutChart data={distanceData} labels={serviceNames} />
             </Card>
           </Col>
-          <Col xs={24} md={8} lg={8} xl={8} >
-            <Card title="Average Duration" style={{marginTop:'10px'}}>
+          <Col xs={24} md={8} lg={8} xl={8}>
+            <Card title="Average Duration" style={{ marginTop: "10px" }}>
               <DonutChart data={durationData} labels={serviceNames} />
             </Card>
           </Col>
-          <Col xs={24} md={8} lg={8} xl={8} style={{marginTop:'10px'}}>
+          <Col xs={24} md={8} lg={8} xl={8} style={{ marginTop: "10px" }}>
             <Card title="Average Price">
               <DonutChart data={priceData} labels={serviceNames} />
             </Card>
           </Col>
         </Row>
       </div>
-      <div  style={{
-          display: "flex",
-          justifyContent: "center",
-          // marginTop: "20px",
-        }}>
-          <Row gutter={10} style={{width : "80%"}}>
-            <Col xs={24} md={12} lg={12} xl={12} style={{marginTop:'10px'}}>
-              <Card style={{
-          minHeight:'100%',
-          maxHeight:'100%',
-          // marginTop:'10px'
-        }}>
-                {/* <h2>Ratings</h2> */}
-                <AverageRatings ratings={data.average_ratings} />
-              </Card>
-            </Col>
-            <Col xs={24} md={12} lg={12} xl={12} style={{marginTop:'10px'}}>
-              <Card style={{
-          minHeight:'100%',
-          maxHeight:'100%',
-          // marginTop:'10px'
-        }}>
-                <LineChart data={data.peak_usage_times_per_service}/>
-              </Card>
-            </Col>
-          </Row>
-      </div>
-      <div  style={{
-          display: "flex",
-          justifyContent: "center",
-          // marginTop: "10px",
-        }}>
-          <Row gutter={10} style={{width : "80%"}}>
-            <Col xs={24} md={12} lg={12} xl={12} style={{marginTop:'10px'}}>
-              <Card style={{
-          minHeight:'100%',
-          maxHeight:'100%',
-          // marginTop:'10px',
-          overflow:'scroll'
-        }}>
-                <h2>Popular Routes</h2>
-                <PopularRoutesList popular_routes={data.popular_routes}/>
-              </Card>
-            </Col>
-            <Col xs={24} md={12} lg={12} xl={12} style={{marginTop:'10px'}}>
-              <Card style={{
-          minHeight:'100%',
-          maxHeight:'100%',
-          // marginTop:'10px',
-          overflow:'scroll'
 
-        }}>
-          <h2>Frequent Service Usage </h2>
-                <ServiceUsageList service_usage={data.service_usage_by_vehicle} />
-              </Card>
-            </Col>
-          </Row>
+      <div style={{ display: "flex", justifyContent: "center",  }}>
+        <Row gutter={10} style={responsiveStyle}>
+          <Col xs={24} md={12} lg={12} xl={12} style={{ marginTop: "10px" }}>
+            <Card style={{ minHeight: "100%", maxHeight: "100%" }}>
+              <AverageRatings ratings={data.average_ratings} />
+            </Card>
+          </Col>
+          <Col xs={24} md={12} lg={12} xl={12} style={{ marginTop: "10px" }}>
+            <Card style={{ minHeight: "100%", maxHeight: "100%" }}>
+              <LineChart data={data.peak_usage_times_per_service} />
+            </Card>
+          </Col>
+        </Row>
+      </div>
+
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <Row gutter={10} style={responsiveStyle}>
+          <Col xs={24} md={12} lg={12} xl={12} style={{ marginTop: "10px" }}>
+            <Card style={{ minHeight: "100%", maxHeight: "100%", overflow: "scroll" }}>
+              <h2>Popular Routes</h2>
+              <PopularRoutesList popular_routes={data.popular_routes} />
+            </Card>
+          </Col>
+          <Col xs={24} md={12} lg={12} xl={12} style={{ marginTop: "10px" }}>
+            <Card style={{ minHeight: "100%", maxHeight: "100%", overflow: "scroll" }}>
+              <h2>Frequent Service Usage</h2>
+              <ServiceUsageList service_usage={data.service_usage_by_vehicle} />
+            </Card>
+          </Col>
+        </Row>
       </div>
     </>
   );
